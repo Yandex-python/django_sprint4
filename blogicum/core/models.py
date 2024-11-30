ef92cd5ddbd3771aed8edc45da0c378a76913cf2
ilyas <ilyashepelev23@gmail.com>
2024-11-30 12:45:51 +0300

    init

--- blogicum/core/models.py
+++ blogicum/core/models.py
@@ -1,17 +1,35 @@
 from django.db import models
 
 
-class PublishedCreatedModel(models.Model):
+class BaseModel(models.Model):
+    """Базовая модель.
+
+    Атрибуты:
+        - is_published: Поле для указания статуса публикации.
+        - created_at: Поле, автоматически устанавливающее дату и время
+        создания записи.
+    """
+
     is_published = models.BooleanField(
-        blank=False,
         default=True,
-        verbose_name='Опубликовано',
-        help_text='Снимите галочку, чтобы скрыть публикацию.'
+        verbose_name="Опубликовано",
+        help_text="Снимите галочку, чтобы скрыть публикацию.",
     )
     created_at = models.DateTimeField(
-        blank=False,
         auto_now_add=True,
-        verbose_name='Добавлено'
+        verbose_name="Добавлено",
+    )
+
+    class Meta:
+        abstract = True
+
+
+class BaseTitle(models.Model):
+    """Базовая модель заголовка."""
+
+    title = models.CharField(
+        max_length=256,
+        verbose_name="Заголовок",
     )
 
     class Meta:

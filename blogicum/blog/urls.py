ef009dcf66dfdd63b347945e2588ace7c83630d5
Ilya <76070836+GentIeman@users.noreply.github.com>
2024-12-21 10:28:29 +0300

    Update urls.py

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,71 +5,58 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    # Главная.
     path(
         "",
         views.MainPostListView.as_view(),
         name="index",
     ),
-    # Категория.
     path(
         "category/<slug:category_slug>/",
         views.CategoryPostListView.as_view(),
         name="category_posts",
     ),
-    # Посты опубликованные определенным пользователем.
-    # Для владельца страницы присутствует меню перехода на страницу
-    # редактированию профиля и страницу смены пароля.
     path(
         "profile/<slug:username>/",
         views.UserPostsListView.as_view(),
         name="profile",
     ),
-    # Пост.
     path(
-        "posts/<int:pk>/",
+        "posts/<int:post_id>/",
         views.PostDetailView.as_view(),
         name="post_detail",
     ),
-    # Редактировать профиля пользователя.
     path(
         "edit_profile/",
         views.UserProfileUpdateView.as_view(),
         name="edit_profile",
     ),
-    # Создать пост.
     path(
         "posts/create/",
         views.PostCreateView.as_view(),
         name="create_post",
     ),
-    # Редактировать пост.
     path(
-        "posts/<int:pk>/edit/",
+        "posts/<int:post_id>/edit/",
         views.PostUpdateView.as_view(),
         name="edit_post",
     ),
-    # Удалить пост.
     path(
-        "posts/<int:pk>/delete/",
+        "posts/<int:post_id>/delete/",
         views.PostDeleteView.as_view(),
         name="delete_post",
     ),
-    # Добавить комментарий.
     path(
-        "posts/<int:pk>/comment/",
+        "posts/<int:post_id>/comment/",
         views.CommentCreateView.as_view(),
         name="add_comment",
     ),
-    # Редактировать комментарий.
     path(
-        "posts/<int:pk>/edit_comment/<int:comment_pk>/",
+        "posts/<int:post_id>/edit_comment/<int:comment_id>/",
         views.CommentUpdateView.as_view(),
         name="edit_comment",
     ),
-    # Удалить комментарий
     path(
-        "posts/<int:pk>/delete_comment/<int:comment_pk>/",
+        "posts/<int:post_id>/delete_comment/<int:comment_id>/",
         views.CommentDeleteView.as_view(),
         name="delete_comment",
     ),
